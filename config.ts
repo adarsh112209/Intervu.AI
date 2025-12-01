ef0264344fde6412db1e453274aa37fa7c21ee7a
Adarsh Tiwari <adarshpandat100@gmail.com>
2025-12-01 16:18:27 +0530

    added build

--- config.ts
+++ config.ts
@@ -1,4 +1,1 @@
-
-// TODO: Replace this string with your actual Google Gemini API Key
-// You can get one at https://aistudio.google.com/app/apikey
 export const API_KEY = 'AIzaSyDKV_ZjfhfwuqRXeo_dFzLZUOFwO3X5YYE';
